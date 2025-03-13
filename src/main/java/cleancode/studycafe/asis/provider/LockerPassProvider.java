337f0f26750e6337eb12d36f63185e0b86d5e849
Chaeruin <cms3604@naver.com>
2025-03-13 13:01:19 +0900

    refactor

---
+++ src/main/java/cleancode/studycafe/asis/provider/LockerPassProvider.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.asis.provider;
+
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPasses;
+
+public interface LockerPassProvider {
+
+    StudyCafeLockerPasses getLockerPasses();
+
+}
---
+++ src/main/java/cleancode/studycafe/asis/provider/SeatPassProvider.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.asis.provider;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPasses;
+
+public interface SeatPassProvider {
+
+    StudyCafeSeatPasses getSeatPasses();
+
+}
